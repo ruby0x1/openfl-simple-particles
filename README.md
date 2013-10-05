d5a981678f3bbec46460898f6231c554e17ab0c2
underscorediscovery <sven@underscorediscovery.com>
2013-10-04 23:10:41 -0230

    Adding initial commit and instructions and example

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 openfl-simple-particles
 =======================
 
-A simple particle system for openfl
+A simple particle system for openfl.
+
+[ 1.0.0 Demo ](http://underscorediscovery.com/sven/openfl-simple-particles/).
+
+Usage : 
+
+	- Copy the particles/ folder into your source folder.
+	- Create a new particle system, 
+	- `var system : ParticleSystem = new ParticleSystem( new flash.geom.Point() );`
+	- Add an emitter 
+	- `system.add_emitter('smoke1', { particle_image : openfl.Assets.getBitmapData("assets/smoke.png") });`
+	- Start the system emitting
+	- `system.emit( duration_in_seconds ); //default is -1, infinite emit
+	- Add to the stage
+	- `addChild( system );`
+
+Known Issues :
+
+	- Supports color internally but does not apply it to the sprite (only alpha is applied atm)
+	- Rotation needs some work, setting a start rotation and no end rotation means start -> end tween, if end is not set explicitly it should ignore
+
+License and more :
+
+	- Ported more than once from some js file online (that I found years ago), I don't recall the exact source
+	- Ported from original code more than once to diff js libraries, then to haxe and then to openfl
+	- MIT license
+
+
+Feel free to pull request or post issues!
+
---
+++ example/Source/Main.hx
@@ -0,0 +1,89 @@
+package;
+
+
+import flash.display.Bitmap;
+import flash.display.BitmapData;
+import flash.display.Sprite;
+import flash.events.MouseEvent;
+import flash.geom.Point;
+
+import openfl.Assets;
+
+import particles.Particles.ParticleSystem;
+
+class Main extends Sprite {
+	
+	var system : ParticleSystem;
+	
+//Construct 
+
+	public function new () {
+		
+		super();
+
+			//listen for events
+		addEventListener( flash.events.Event.ENTER_FRAME, onupdate );		
+		stage.addEventListener( flash.events.MouseEvent.MOUSE_MOVE, onmousemove );
+		stage.addEventListener( flash.events.MouseEvent.MOUSE_DOWN, onmousedown );
+
+			//disallow large dt values 
+		enddt = haxe.Timer.stamp();
+
+		var smoke1 = Assets.getBitmapData("assets/smoke.png");
+		var smoke2 = Assets.getBitmapData("assets/smoke2.png");
+				
+				//create particle system at the middle screen
+			system = new ParticleSystem(new Point(stage.stageWidth/2, stage.stageWidth/2));
+				
+				//create an emitter. template is a dynamic with lots of options
+			system.add_emitter('smoke1', { particle_image : smoke1, emit_time:0.1, life:1 });
+				//a second emitter, varying the smoke up
+			system.add_emitter('smoke2', { 
+				particle_image : smoke2, 
+				emit_time:1, 
+				gravity : new Point(0,-5),
+				rotation_random:0, 
+				end_rotation_random:0, 
+				end_size:new Point(256,256) 
+			});
+
+				//start emitting
+			system.emit();	
+
+			//add to the stage
+		addChild(system);
+	}
+		
+//Mouse
+
+		//Click to toggle emission
+	function onmousedown(e) {
+		if(system.active) {
+			system.stop();
+		} else {
+			system.emit();
+		}
+	}
+
+		//mouse position changes the system position
+	function onmousemove(e:MouseEvent) {
+		system.pos.setTo(e.stageX, e.stageY);
+	}
+
+//Update
+
+	var dt : Float = 0.016;
+	var enddt : Float = 0;
+
+	function onupdate(e) {
+
+			//calculate the delta time
+		dt = haxe.Timer.stamp() - enddt;
+			//store the end of the frame time.
+		enddt = haxe.Timer.stamp();
+			//update the particle system
+		system.update(dt);
+
+	}
+	
+}
