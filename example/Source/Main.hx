bc15daf9ab0e51dcaa65e469e89298383295c945
underscorediscovery <sven@underscorediscovery.com>
2013-10-04 23:48:41 -0230

    fixing spaces

--- example/Source/Main.hx
+++ example/Source/Main.hx
@@ -12,66 +12,66 @@ import openfl.Assets;
 import particles.Particles.ParticleSystem;
 
 class Main extends Sprite {
-	
-	var system : ParticleSystem;
-	
+    
+    var system : ParticleSystem;
+    
 //Construct 
 
-	public function new () {
-		
-		super();
+    public function new () {
+        
+        super();
 
-			//listen for events		
-		stage.addEventListener( flash.events.MouseEvent.MOUSE_MOVE, onmousemove );
-		stage.addEventListener( flash.events.MouseEvent.MOUSE_DOWN, onmousedown );
+            //listen for events     
+        stage.addEventListener( flash.events.MouseEvent.MOUSE_MOVE, onmousemove );
+        stage.addEventListener( flash.events.MouseEvent.MOUSE_DOWN, onmousedown );
 
-		
+        
 
-		var smoke1 = Assets.getBitmapData("assets/smoke.png");
-		var smoke2 = Assets.getBitmapData("assets/smoke2.png");
-				
-				//create particle system at the middle screen
-			system = new ParticleSystem(new Point(stage.stageWidth/2, stage.stageWidth/2) );
-				
-				//create an emitter. template is a dynamic with lots of options
-			system.add_emitter('smoke1', { particle_image : smoke1, emit_time:0.1, life:1 });
-				//a second emitter, varying the smoke up
-			system.add_emitter('smoke2', { 
-				particle_image : smoke2, 
-				emit_time:1, 
-				gravity : new Point(0,-5),
-				rotation_random:0, 
-				end_rotation_random:0, 
-				end_size:new Point(256,256) 
-			});
+        var smoke1 = Assets.getBitmapData("assets/smoke.png");
+        var smoke2 = Assets.getBitmapData("assets/smoke2.png");
+                
+                //create particle system at the middle screen
+            system = new ParticleSystem(new Point(stage.stageWidth/2, stage.stageWidth/2) );
+                
+                //create an emitter. template is a dynamic with lots of options
+            system.add_emitter('smoke1', { particle_image : smoke1, emit_time:0.1, life:1 });
+                //a second emitter, varying the smoke up
+            system.add_emitter('smoke2', { 
+                particle_image : smoke2, 
+                emit_time:1, 
+                gravity : new Point(0,-5),
+                rotation_random:0, 
+                end_rotation_random:0, 
+                end_size:new Point(256,256) 
+            });
 
-				//start emitting
-			system.emit();	
+                //start emitting
+            system.emit();  
 
-			//add to the stage
-		addChild(system);
+            //add to the stage
+        addChild(system);
 
-	}
-		
+    }
+        
 //Mouse
 
-		//Click to toggle emission
-	function onmousedown(e) {
-		if(system.active) {
-			system.stop();
-		} else {
-			system.emit();
-		}
-	}
+        //Click to toggle emission
+    function onmousedown(e) {
+        if(system.active) {
+            system.stop();
+        } else {
+            system.emit();
+        }
+    }
 
-		//mouse position changes the system position
-	function onmousemove(e:MouseEvent) {
-		system.pos.setTo(e.stageX, e.stageY);
-	}
+        //mouse position changes the system position
+    function onmousemove(e:MouseEvent) {
+        system.pos.setTo(e.stageX, e.stageY);
+    }
 
 //Update
 
 
-	
-	
+    
+    
 }
